ce909e651adce225bb5f79fc6e9f195c8a4a4132
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-31 22:49:18 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,5 +9,8 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(input_to_index)
+  if(input_to_index.to_i < 1 && input_to_index > 9)
+    input_to_index = -1
+  end
   return input_to_index.to_i
 end
