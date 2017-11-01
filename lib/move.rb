5a174bb769046026d53df9e3b9d900ba80197d69
Margaretha Juan <tulip_holy@yahoo.com>
2017-11-01 00:20:31 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,6 @@ def input_to_index(input_to_index)
   return input_to_index
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def move(board = board, index = -1, value = "X")
   if(index > -1 && index < 9)
     board[index] = value;
